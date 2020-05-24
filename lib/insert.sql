2f04ba4049158939b1cc714fc34dfc719b0ad565
Dina Kendle <dina.kendle@gmail.com>
2020-05-24 17:28:30 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,4 +6,3 @@ INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ()
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ()
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ()
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ()
-
