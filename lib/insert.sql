9493c35bea58837b02d0fe11807cd65e6d8cf523
Dina Kendle <dina.kendle@gmail.com>
2020-05-24 17:53:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Mr. Chocolate", 2, "Male","Brown", "Passive", "Yes")
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Rowdy",5,"Female", "White","Happy","Yes")
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Tabitha", 1, "Female","Brown", "Angry", "Yes")
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Sergeant Brown",10,"Male", "Black", "Stoic", "Yes")
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Melissa", 4, "Non Bearinary", "Blossoming Brown","Yes")
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Grinch", 9, "Male","Grey","Grumpy","Yes")
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Wendy", 3, "Female", "Grey Brown", "Yes")
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES (NULL, 2, "Male", "Black", Yes)
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Mr. Chocolate", 2, "Male","Brown", "Passive", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Rowdy",5,"Female", "White","Happy","Yes");
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Tabitha", 1, "Female","Brown", "Angry", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Sergeant Brown",10,"Male", "Black", "Stoic", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Melissa", 4, "Non Bearinary", "Blossoming Brown","Yes");
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Grinch", 9, "Male","Grey","Grumpy","Yes");
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Wendy", 3, "Female", "Grey Brown", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES (NULL, 2, "Male", "Black", "Timid","Yes");
