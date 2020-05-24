d5adcaa0f7db56bbb540bd98ebbe488382dcec8a
Dina Kendle <dina.kendle@gmail.com>
2020-05-24 17:54:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Mr. Cho
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Rowdy",5,"Female", "White","Happy","Yes");
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Tabitha", 1, "Female","Brown", "Angry", "Yes");
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Sergeant Brown",10,"Male", "Black", "Stoic", "Yes");
-INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Melissa", 4, "Non Bearinary", "Blossoming Brown","Gentle","Yes");
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Melissa", 4, "Female", "Blossoming Brown","Gentle","Yes");
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Grinch", 9, "Male","Grey","Grumpy","Yes");
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ("Wendy", 3, "Female", "Grey Brown", "Kind","Yes");
 INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES (NULL, 2, "Male", "Black", "Timid","Yes");
